57a12a511658f43c6363731845f9d73d0e81e672
Hao <xpdinghao@163.com>
2021-12-02 23:10:44 +0100

    first commit in visual studio

---
+++ Restaurant/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to the XX Restaurant!");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1. Check free tables with a certain time");
+            Console.WriteLine("2. Make a reservation");
+            Console.WriteLine("3. Check the daily Menu");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("What can I do for you? Please select one from the above (input a number and then press ENTER)");
+
+        }
+    }
+}
---
+++ Restaurant/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Restaurant
+{
+    public class Table
+    {
+        public Table()
+        {
+            int table_id;
+
+        }
+    }
+}
